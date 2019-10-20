63df6d3eafd1ed3774434d25c9d89084c4bea7dc
Brandon Welsh <contact@brandonarmand.com>
2019-10-20 03:15:36 -0700

    added cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 app.disable('etok')
 app.use(bodyParser.text());
+app.use(cors())
 
 appConfig.init(app);
 routeConfig.init(app);
